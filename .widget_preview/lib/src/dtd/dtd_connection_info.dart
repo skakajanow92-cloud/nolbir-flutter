78f56cbcbf0a563db97bd2048c5bff1baa1396a8
Sohbet Kakajanow <skakajanow92@gmail.com>
2026-09-16 21:30:57 +0500

    gitignore modified linux

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:41231/tL0qxpkxFCk=';
+const String kWidgetPreviewService =
+    'widget-preview-7177a320-f392-4389-8a92-332a1d599dfc';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-7177a320-f392-4389-8a92-332a1d599dfc';
+const String kProjectRootPath = r'/home/vboxuser/Desktop/nolbir-flutter';
